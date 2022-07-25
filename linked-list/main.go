b0aa0d5693b8cd1544a549627395a8ae997f20c7
Asgun-alt <55643003+Asgun-alt@users.noreply.github.com>
2022-07-25 11:16:09 +0700

    Linked lists are linear data structures that hold data in individual objects called nodes.

--- linked-list/main.go
+++ linked-list/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "fmt"
 
 type node struct {
 	data int
@@ -18,14 +19,56 @@ func (l *linkedList) prepend(n *node){
 	l.length++
 }
 
+func (l linkedList) printListData(){
+	toPrint := l.head
+
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Print("\n")
+}
+
+func (l *linkedList) deleteWithValue(value int){
+	// Handling the empty list case
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	prevToDelete := l.head
+	for prevToDelete.next.data != value {
+		if prevToDelete.next.next == nil {
+			return
+		}
+		prevToDelete = prevToDelete.next
+	}
+	prevToDelete.next = prevToDelete.next.next
+	l.length--
+}
+
 func main(){
 	list := linkedList{}
 	node1 := &node{data:48}
 	node2 := &node{data:99}
-	node3 := &node{data:12}
+	node3 := &node{data:20}
+	node4 := &node{data:9}
+	node5 := &node{data:60}
+	node6 := &node{data:73}
 	list.prepend(node1)
 	list.prepend(node2)
 	list.prepend(node3)
+	list.prepend(node4)
+	list.prepend(node5)
+	list.prepend(node6)
 
-	fmt.Println(list)
+	list.printListData()
+	list.deleteWithValue(9)
+	list.printListData()
 }
